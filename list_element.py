bbbb09e1118595a2d7d6612b75568b36b07d2478
Nikolaos Zisos <nikoszisos@gmail.com>
2026-01-21 20:32:42 +0200

    Initial commit

---
+++ list_element.py
@@ -0,0 +1,6 @@
+my_list = [1,55,7,93]
+print (my_list)
+my_list[3] = 77
+print (my_list)
+print (my_list[0])
+print (my_list[1])
---
+++ lists.py
@@ -0,0 +1,9 @@
+list_int = [1,2,3,4]
+list_float = [0.1, 1.3, 2.4]
+list_string = ["Nikos", "Kostas"]
+list_sylogi = ["Nikos", 0.1, 2, [1,2,3,4]]
+
+print (list_int)
+print (list_float)
+print (list_sylogi)
+print (type(list_sylogi))
