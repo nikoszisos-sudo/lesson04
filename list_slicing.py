f1a98ea40f17b3100c76193e4858f2fa1d3311be
Nikolaos Zisos <nikoszisos@gmail.com>
2026-01-24 20:35:08 +0200

    ok

---
+++ list_slicing.py
@@ -0,0 +1,14 @@
+my_list = [1, 2, 4, 5, 9]
+print (my_list)
+my_list1 = my_list[2:4]
+print (my_list1)
+my_list2 = my_list[1:]
+print (my_list2)
+my_list3 = my_list[:4]
+print (my_list3)
+my_list4 = my_list[:]
+print (my_list4)
+my_list5 = my_list[-4:-1]
+print (my_list5)
+my_list6 = my_list[-4:]
+print (my_list6)
---
+++ reverse_indexing.py
@@ -0,0 +1,9 @@
+lista = [1, 5, 7, 2]
+print (lista)
+lista[0] = 3
+print (lista)
+print (lista[-1])
+print (lista[-4])
+lista[-3] = 11
+print (lista)
+
