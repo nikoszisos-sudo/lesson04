358f3079977888839ca685560637c42537d42e02
Nikolaos Zisos <nikoszisos@gmail.com>
2026-01-25 20:54:15 +0200

    ok

---
+++ pop_remove_clear.py
@@ -0,0 +1,17 @@
+my_list = [55, 28, 20]
+last = my_list.pop()
+print (last)
+print (my_list)
+first = my_list.pop(0)
+print (first)
+print (my_list)
+
+my_list1 = [60, 98, 44, 0, 44]
+print (my_list1)
+my_list1.remove(44)
+print (my_list1)
+
+my_list2 = [70, 66, 12]
+print (my_list2)
+my_list2.clear()
+print (my_list2)
