0e0ba6f71296de99e0ec901cefb2f42395e264be
Nikolaos Zisos <nikoszisos@gmail.com>
2026-01-25 17:57:53 +0200

    ok

---
+++ mpros_pisw.py
@@ -0,0 +1,12 @@
+my_list = [1, 2, 4, 5, 9, 11, 87, 32, 19, 90]
+print (my_list[2])
+print (my_list[3])
+print (my_list[4])
+print (my_list[5])
+
+print (my_list[-8])
+print (my_list[-7])
+print (my_list[-6])
+print (my_list[-5])
+
+print (my_list[2:6])
