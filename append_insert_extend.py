727f3d32cad77ec488d4780089f9fed6fca84655
Nikolaos Zisos <nikoszisos@gmail.com>
2026-01-25 20:31:46 +0200

    ok

---
+++ append_insert_extend.py
@@ -0,0 +1,11 @@
+my_list = [55, 28, 20]
+my_list.append(24)
+print (my_list)
+
+my_list = [55, 28, 20]
+my_list.insert(0, 24)
+print (my_list)
+
+my_list = [55, 28, 20]
+my_list.extend([0, 1])
+print (my_list)
