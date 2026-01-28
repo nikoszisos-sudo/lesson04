2e9a4ff3fed8997d686961e8e7c12c317bf0c7ee
Nikolaos Zisos <nikoszisos@gmail.com>
2026-01-28 20:50:00 +0200

    ok

---
+++ cash_back.py
@@ -0,0 +1,17 @@
+cash_desk = []
+cash_desk.append("Tom")
+cash_desk.append("Bob")
+print (cash_desk)
+
+first = cash_desk.pop(0)
+print (first + " εξυπηρετηθηκε")
+
+cash_desk.append("Pam")
+cash_desk.append("Jim")
+print (cash_desk)
+
+first = cash_desk.pop(0)
+print (first + " εξυπηρετηθηκε")
+print (cash_desk)
+
+
