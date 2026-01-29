7ac0329f37d3e70f8e53a6cdb3542c4ceaf56bf2
Nikolaos Zisos <nikoszisos@gmail.com>
2026-01-29 21:16:03 +0200

    ok

---
+++ taksinomisi_pragmatikwn.py
@@ -0,0 +1,6 @@
+x = float(input("Δώσε τον πρώτο αριθμό: "))
+y = float(input("Δώσε τον δεύτερο αριθμό: "))
+z = float(input("Δώσε τον τρίτο αριθμό: "))
+my_list = [x,y,z]
+my_list.sort()
+print (my_list)
